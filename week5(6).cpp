91160990d125ff9367dcd70c79a69425c42ac1c8
zoyakhanalig1-create <zoyakhanalig1@gmail.com>
2025-09-12 13:41:04 +0530

    Create week5(6).cpp

---
+++ week5(6).cpp
@@ -0,0 +1,11 @@
+/*Week-5 Task #6
+C Program to find out the ASCII value of a character.*/
+
+#include<stdio.h>
+
+main() {
+        char character;
+        printf("Enter any character: \n");
+        scanf("%c", &character);
+        printf("The ASCII value of \'%c\' is %d\n", character, character);
+}
